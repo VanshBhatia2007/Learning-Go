6b6ee9e679d9a0848df530ee63a912206be88fd8
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2025-06-30 18:07:21 +0530

    Added day02 progress

---
+++ day02/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//strings
+	fmt.Println("hello")
+	//int
+	fmt.Println(10)
+	fmt.Println(10 + 10)
+	//floats
+	fmt.Println(10.5 / 0.5)
+	//bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/VanshBhatia2007/learning-go
-
-go 1.24.4
